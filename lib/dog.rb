9e2d2d68f584e2c5e6ca70eea4fe639f8df4f82f
Cliff Crelin <cliff.crelin@gmail.com>
2020-01-11 02:09:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,17 @@
 class Dog
- attr_accessor :owner 
+ attr_accessor :owner, :mood 
  attr_reader :name 
- def initialize(name, owner)
+ 
+ @@all = []
+ 
+ def initialize(name, owner, mood="nervous")
    @name = name 
    @owner = owner
+   @mood = mood
+   @@all << self 
+ end
+ 
+ def self.all
+   @@all 
  end 
 end
